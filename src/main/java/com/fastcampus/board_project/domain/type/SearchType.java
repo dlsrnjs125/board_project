319758d59f53cdef35481f4e07356810e522fc1a
inkwon <dlsrnjs125@naver.com>
2023-07-14 11:32:47 +0900

    #20 - 게시글 서비스 로직의 테스트와 골격 잡기
    다음의 기능을 테스트로 표현
    
    * 게시글 검색하면 게시글 리스트 반환
    * 게시글을 ID로 조회
    * 게시글 생성
    * 게시글 수정
    * 게시글 삭제
    
    필요한 dto 와 enum을 함께 작성했는데,
    추후에 수정할 수도 있음

---
+++ src/main/java/com/fastcampus/board_project/domain/type/SearchType.java
@@ -0,0 +1,5 @@
+package com.fastcampus.board_project.domain.type;
+
+public enum SearchType {
+    TITLE, CONTENT, ID, NICKNAME, HASHTAG
+}
---
+++ src/main/java/com/fastcampus/board_project/service/ArticleService.java
@@ -0,0 +1,37 @@
+package com.fastcampus.board_project.service;
+
+import com.fastcampus.board_project.domain.type.SearchType;
+import com.fastcampus.board_project.dto.ArticleDto;
+import com.fastcampus.board_project.dto.ArticleUpdateDto;
+import com.fastcampus.board_project.repository.ArticleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Transactional
+@Service
+public class ArticleService {
+
+    private final ArticleRepository articleRepository;
+
+    @Transactional(readOnly = true)
+    public Page<ArticleDto> searchArticles(SearchType searchType, String searchKeyword) {
+        return Page.empty();
+    }
+
+    @Transactional(readOnly = true)
+    public ArticleDto searchArticle(long l) {
+        return null;
+    }
+
+    public void saveArticle(ArticleDto dto) {
+    }
+
+    public void updateArticle(long articleId, ArticleUpdateDto dto) {
+    }
+
+    public void deleteArticle(long articleId) {
+    }
+}
