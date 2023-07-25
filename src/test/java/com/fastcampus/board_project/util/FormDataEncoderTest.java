623a98d82a7125b2dcfb5832597b61afdad5dd93
inkwon <dlsrnjs125@naver.com>
2023-07-25 19:39:58 +0900

    #65 - JUnit5 테스트에서 불필요한 public 접근자 삭제
    공식문서에 의하면 JUnit5 단위 테스트는
    클래스와 메소드에 `public` 접근자를 필요로 하지 않는다.
    전체 테스트를 검사하여 삭제해줌.

--- src/test/java/com/fastcampus/board_project/util/FormDataEncoderTest.java
+++ src/test/java/com/fastcampus/board_project/util/FormDataEncoderTest.java
@@ -22,7 +22,7 @@ class FormDataEncoderTest {
 
     private final FormDataEncoder formDataEncoder;
 
-    public FormDataEncoderTest(@Autowired FormDataEncoder formDataEncoder) {
+    FormDataEncoderTest(@Autowired FormDataEncoder formDataEncoder) {
         this.formDataEncoder = formDataEncoder;
     }
 
