b272603751dbd10f4ec26a474027b6b4f874b84b
inkwon <dlsrnjs125@naver.com>
2024-07-21 20:15:03 +0900

    #119 - 어드민 프로젝트에서 사용할 data rest api 들에 id 필드를 오픈
    `UserAccount`를 노출할 api로 추가해 줌.
    이렇게 하면 다른 게시글, 댓글 등의 data rest api 노출 데이터 형식이
    원하지 않는 스타일로 바뀌게 된다.
    이를 수동으로 설정하기 위해 projection 기능을 사용하여
    노출할 필드들을 직접 지정함.

--- src/main/java/com/fastcampus/board_project/config/DataRestConfig.java
+++ src/main/java/com/fastcampus/board_project/config/DataRestConfig.java
@@ -1,16 +1,24 @@
 package com.fastcampus.board_project.config;
 
+import com.fastcampus.board_project.domain.Article;
+import com.fastcampus.board_project.domain.ArticleComment;
+import com.fastcampus.board_project.domain.Hashtag;
 import com.fastcampus.board_project.domain.UserAccount;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 
+@Configuration
 public class DataRestConfig {
 
     @Bean
     public RepositoryRestConfigurer repositoryRestConfigurer() {
         return RepositoryRestConfigurer.withConfig((config, cors) ->
-                config.exposeIdsFor(UserAccount.class)
+                config
+                        .exposeIdsFor(UserAccount.class)
+                        .exposeIdsFor(Article.class)
+                        .exposeIdsFor(ArticleComment.class)
+                        .exposeIdsFor(Hashtag.class)
         );
     }
 
